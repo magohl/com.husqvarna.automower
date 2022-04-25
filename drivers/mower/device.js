e4cefc4027e68b545952a28651caedb222bd99ac
Theodor Storm <theodor@tstorm.se>
2022-04-25 22:58:42 +0200

    Fixed some issues when having multiple Automower devices

--- drivers/mower/device.js
+++ drivers/mower/device.js
@@ -22,8 +22,6 @@ module.exports = class MowerDevice extends Homey.Device {
     this._timerId = null;
     this._pollingInterval = this.getSettings().polling_interval * 60000;
 
-    this.initFlows();
-
     if (eval(this.getSettings().polling))
       this.refreshCapabilities();
   }
@@ -79,116 +77,6 @@ module.exports = class MowerDevice extends Homey.Device {
       this.addCapability(capability);
     }
   }
-
-  async initFlows() {
-    this.log('MowerDevice initalize flows');
-
-    /* Action 'pause' */
-    this.homey.flow.getActionCard('pause')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice FlowAction pause triggered');
-        let id = this.getData().id;
-        let data = {
-          data: {
-            type: 'ParkUntilFurtherNotice'
-          }
-        };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Action 'ParkUntilNextSchedule' */
-    this.homey.flow.getActionCard('park_until_next_schedule')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice FlowAction park_until_next_schedule triggered');
-        let id = this.getData().id;
-        let data = {
-          data: {
-            type: 'ParkUntilNextSchedule'
-          }
-        };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Action 'ParkUntilFurtherNotice' */
-    this.homey.flow.getActionCard('park_until_further_notice')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice FlowAction park_until_further_notice triggered');
-        let id = this.getData().id;
-        let data = {
-          data: {
-            type: 'ParkUntilFurtherNotice'
-          }
-        };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Action 'Park' */
-    this.homey.flow.getActionCard('park_duration')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice FlowAction park_duration triggered');
-        let duration = args.hours;
-        let id = this.getData().id;
-        let data = {
-          data: {
-            type: 'Park',
-            attributes: {
-              duration: duration * 60
-            }
-          }
-        };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Action 'ResumeSchedule' */
-    this.homey.flow.getActionCard('resume_schedule')
-      .registerRunListener(async (args) => {
-        this.log('MowerDevice FlowAction resume_schedule triggered');
-        let id = this.getData().id;
-        let data = {
-          data: {
-            type: 'ResumeSchedule'
-          }
-        };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Action 'Start' */
-    this.homey.flow.getActionCard('start_duration')
-    .registerRunListener(async (args, state) => {
-      this.log('MowerDevice Flowaction start_duration triggered');
-      let duration = args.hours;
-      let id = this.getData().id;
-      let data = {
-        data: {
-          type: 'Start',
-          attributes: {
-            duration: duration * 60
-          }
-        }
-      };
-      let actionResult = await this.util.sendMowerAction(id, data);
-      return actionResult;
-    });
-
-    /* Condition 'activity_is' */
-    this.homey.flow.getConditionCard('activity_is')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice Flow-condition activity_is triggered');
-        return (args.activity === this.getCapabilityValue('mower_activity_capability'));
-      });  
-
-    /* Condition 'state_is' */
-    this.homey.flow.getConditionCard('state_is')
-      .registerRunListener(async (args, state) => {
-        this.log('MowerDevice Flow-condition state_is triggered');
-        return (args.state === this.getCapabilityValue('mower_state_capability'));
-      });
-  }
   
   async refreshCapabilities() {
     try
@@ -214,6 +102,7 @@ module.exports = class MowerDevice extends Homey.Device {
     }
     catch ( err ) {
         this.log( "MowerDevice refresh error: " + err );
+        this.log(err.stack);
         this.setWarning( "error getting mower status", null );
     }
 
